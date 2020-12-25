8ae5194729aff00bef10008eaedab982fddef9f9
aaaaaddf <hurumura1120@icloud.com>
2020-12-25 16:04:01 +0900

    ci

--- config/database.php
+++ config/database.php
@@ -90,6 +90,20 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+        'circle_testing' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'sharety_test',
+            'username' => 'root',
+            'password' => 'dbpass',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
 
     ],
 
